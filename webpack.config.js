8e29dbc9acae9d303059b624122acec1e79329df
Dettlaff <maciekwiatr17@gmail.com>
2020-05-11 17:55:00 +0200

    Fix dependencies, add rest spread plugin

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: ["@babel/preset-env"],
+                        plugins: ["@babel/plugin-proposal-object-rest-spread"],
                     },
                 },
             },
